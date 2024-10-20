10fa7e652f0415629591c3237828185ac1afb90a
Xu Chen <50715483+xuchen312@users.noreply.github.com>
2024-10-20 15:41:54 +0100

    Update spm_clusterTDP.m
    Add more input checks.

--- spm_clusterTDP.m
+++ spm_clusterTDP.m
@@ -6,11 +6,12 @@ function varargout = spm_clusterTDP(varargin)
 % FORMAT:                          spm_clusterTDP([xSPM,file])
 %         [hReg,xSPM,SPM,TabDat] = spm_clusterTDP([xSPM,file])
 % -------------------------------------------------------------------------
-% Inputs (optional):
+% Inputs (optional; if empty or not specified, the default is used):
 %  -   xSPM: an input structure containing SPM, distribution & filtering
-%            details (see spm_getSPM.m for details) 
+%            details (see spm_getSPM.m for details; default: compute xSPM
+%            interactively) 
 %  -   file: a character array specifying the output name for a CSV file
-%            (e.g. ClusTab.csv)
+%            (default: output table is not saved)
 %
 % Outputs (optional, for interactive exploration):
 %  -   hReg: handle of MIP XYZ registry object 
@@ -36,24 +37,44 @@ end
 
 %-Check input arguments
 %----------------------------------------------------------------------
+if nargin > 2; error('Too many input arguments'); end
 if nargin > 1
-    if isstruct(varargin{1})
-        xSPM = varargin{1};
-    else
-        error('Wrong input format: ''xSPM'' should be a structure.');
+    if ~isempty(varargin{1})
+        if isstruct(varargin{1})
+            xSPM = varargin{1};
+        else
+            error('The 1st argument must be ''xSPM''');
+        end
     end
-    if ischar(varargin{2})
-        file = varargin{2};
-    else
-        error('Wrong input format: ''file'' should be a character array.');
+    if ~isempty(varargin{2})
+        if ischar(varargin{2})
+            file = varargin{2};
+        else
+            error('The 2nd argument must be ''file''');
+        end
     end
-elseif nargin == 1
-    if isstruct(varargin{1})
-        xSPM = varargin{1};
-    elseif ischar(varargin{1})
-        file = varargin{1};
-    else
-        error('Unrecognised input format.');
+end
+if nargin == 1
+    if ~isempty(varargin{1})
+        if isstruct(varargin{1})
+            xSPM = varargin{1};
+        elseif ischar(varargin{1})
+            file = varargin{1};
+        else
+            error(['Unrecognised input: ' varargin{1}]);
+        end
+    end
+end
+if exist('file','var')
+    [fpath,fname,fext] = fileparts(file);
+    if isempty(fext)
+        file = strcat(file,'.csv');
+    elseif ~strcmpi(fext,'.csv')
+        error('Unexpected output file extension: %s',fext);
+    end
+    if isempty(fname)
+        file = fullfile(fpath,strcat('ClusTab',fext));
+        warning('Found empty file name and use ''ClusTab.csv''');
     end
 end
 
@@ -76,12 +97,6 @@ spm_clusterTDP_list('TxtList',TabDat);
 %-Check output file name & write result table to a csv file
 %----------------------------------------------------------------------
 if exist('file','var')
-    [~,~,fext] = fileparts(file);
-    if isempty(fext)
-        file = strcat(file,'.csv');
-    elseif ~strcmpi(fext,'.csv')
-        error('Unexpected output file extension: %s',fext);
-    end
     spm_clusterTDP_list('CSVList',TabDat,file);
 end
 
